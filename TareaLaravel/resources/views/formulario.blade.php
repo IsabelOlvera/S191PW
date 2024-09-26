28df754d67acb6520149afdf18b323e0f91fbd89
Isabel Villagran Olvera <112986474+IsabelOlvera@users.noreply.github.com>
2024-09-25 20:33:13 -0600

    Ruta /formulario

---
+++ TareaLaravel/resources/views/formulario.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resourses/js/app.js'])
+    <title> Registro Clientes </title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Turista sin Maps</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Formulario de registro</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Consultar clientes</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</body>
+</html>
