06d1199a57e5639c903d4f47e2931e298f289458
Isabel Villagran Olvera <112986474+IsabelOlvera@users.noreply.github.com>
2024-10-09 20:29:48 -0600

    Avance ComponenteBlade

--- TareaLaravel/routes/web.php
+++ TareaLaravel/routes/web.php
@@ -12,3 +12,5 @@ Route::view('/', 'inicio')->name('rutainicio');
 Route::view('/formulario', 'formulario')->name('rutaformulario');
 
 Route::view('/clientes', 'clientes')->name('consultaclientes');
+
+Route::view('/component', 'componentes')->name('rutacomponent');
