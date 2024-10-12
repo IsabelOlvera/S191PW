5251d73070bf76c758e795deb83f3a8a9ddae75b
Isabel Villagran Olvera <112986474+IsabelOlvera@users.noreply.github.com>
2024-10-11 20:19:18 -0600

    Práctica 4 completa

--- TareaLaravel/routes/web.php
+++ TareaLaravel/routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+Route::get('/',[controladorVistas::class,'home'])->name('rutainicio');
+Route::get('/formulario',[controladorVistas::class,'insert' ])->name('rutaformulario');
+Route::get('/clientes',[controladorVistas::class,'select' ])->name('consultaclientes');
 
 /*Route::get('/', function () {
     return view('clientes');
 });
 */
 
-Route::view('/', 'inicio')->name('rutainicio');
+/*Route::view('/', 'inicio')->name('rutainicio');
 
 Route::view('/formulario', 'formulario')->name('rutaformulario');
 
-Route::view('/clientes', 'clientes')->name('consultaclientes');
+Route::view('/clientes', 'clientes')->name('consultaclientes');*/
 
 Route::view('/component', 'componentes')->name('rutacomponent');
