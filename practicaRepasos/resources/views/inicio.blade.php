cdbfa7a4d58986daac81ddfd235f3fa4625a3b29
Isabel Villagran Olvera <112986474+IsabelOlvera@users.noreply.github.com>
2024-10-18 09:24:23 -0600

    Repaso1 Paso 3 Portada
    Se creo una vista inicio al estilo de una portada con mis datos.

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/js/app.js'])
+    <title>Inicio</title>
+    <style>
+        body, html {
+            text-align: center;
+            padding: 50px;
+        }
+        .container {
+            font-family: Cambria;
+            border: 1px solid #c75dc9;
+            padding: 20px;
+            border-radius: 10px;
+            background-color: #f0f0f0dc;
+        }
+        img {
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class='container'>
+        <img src="{{asset('img/logoupq.png')}}" alt="Logo de la Universidad" width="200">
+        <h1><strong>Universidad Polítecnica de Quéretaro</strong></h1>
+        <h3>Ingeniería en Sistemas Computacionales</h3>
+        <br>
+        <br>
+        <h4><strong>PRÁCTICA DE REPASO 1</strong></h4>
+        <br>
+        <h5>Programación Web</h5>
+        <br>
+        <p>
+            <strong>Alumna: </strong> Villagran Olvera Catalina Isabel
+            <br>
+            <strong>Mtro. </strong> Iván Isay Guerra López
+            <br>
+            <br>
+            <strong>Grupo: </strong> S-191 
+            <br>
+            <strong>Fecha: </strong> {{ date('d F Y') }}
+        </p>
+    </div>
+</body>
+</html>
--- practicaRepasos/routes/web.php
+++ practicaRepasos/routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-Route::view('/','welcome');
+Route::view('/','inicio');
