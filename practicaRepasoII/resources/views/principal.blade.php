41c824f529ad4cf25ca80f5fda87868acca0b3ff
Isabel Villagran Olvera <112986474+IsabelOlvera@users.noreply.github.com>
2024-10-31 23:29:43 -0600

    RepasoII. Acomodo de redacción 2

--- practicaRepasoII/resources/views/principal.blade.php
+++ practicaRepasoII/resources/views/principal.blade.php
@@ -78,11 +78,11 @@
         <div class="card-header">{{__('Noticia Literaria')}}</div>
         <div class="card-body">
             <img src="{{ asset('images/medianoche.jpg') }}" alt="Imagen de la noticia" class="news-image">
-            <h5 class="card-title">{{__('La Biblioteca de la Medianoche')}}</h5>
+            <h5 class="card-title">{{__('La Biblioteca de la Media Noche')}}</h5>
             <p>
                 {{__('¿Por qué “La Biblioteca de la Medianoche”? Este título del libro de Matt Haig tal vez fue escogido por el autor a partir de su consideración de que entre la vida y la muerte hay una biblioteca. La protagonista de la novela, Nora Seed, aparece a unas horas de su muerte en una biblioteca: La biblioteca de la medianoche. Un lugar que contiene infinitos libros, que se convierten en infinitas opciones de transitar por otras nuevas vidas que quizá en su vida real no vivió porque se sentía frustrada o creía que nadie la necesitaba. Cada libro de esa biblioteca, le brinda una nueva oportunidad que le permite hacer las cosas bien. Frente a la depresión de Nora, el autor nos va llevando hacia una reflexión personal sobre nuestra propia vida. Y deja muy claro que cada decisión que tomemos a lo largo de ella, por insignificante que sea, influirá por siempre, aunque en el transitar no hagamos altos en el camino para reflexionar sobre nuestros arrojos, y no seamos conscientes de las consecuencias.')}}
             </p>
-            <a href="https://vanguardia.com.mx/opinion/sobre-la-biblioteca-de-la-medianoche-GB1399877" target="_blank" class="btn btn-vintage">Ver más</a>
+            <a href="https://vanguardia.com.mx/opinion/sobre-la-biblioteca-de-la-medianoche-GB1399877" target="_blank" class="btn btn-vintage">{{__('Ver más...')}}</a>
         </div>
     </div>
 </div>
